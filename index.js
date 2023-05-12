810194f8aa0574ce4478e2897675f60a2e46daf8
AholanLate <lauri.ahola2@gmail.com>
2023-05-12 13:29:16 +0300

    nodemon, mongoose, express ym. "trips" model and schema

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const http = require('http');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+
+//MongoDB connection
+const mongoose = require('mongoose');
+const mongoURL = 'mongodb+srv://bikeappuser:bikeapppw@bikeapp.cuw2ogy.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 const port = 4000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Bikeapp');
-});
+app.get("/", (req, res) => {
+    res.send("homepage");
+  })
+  
+app.get("/trips", (req, res) => {
+    res.send("Trips");
+  })
 
-server.listen(port, () => {
-  console.log('Server is running at port ' + port);
+app.get("/stations", (req, res) => {
+    res.send("Stations");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
 });
---
+++ models/trips.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var TripsSchema = new Schema(
+  {
+    departure_time: { type: String, required: true },
+    return_time: { type: String, required: true },
+    departure_station_id: {type: Number, required: true },
+    departure_station_name: { type: String, required: true },
+    return_station_id: {type: Number, required: true },
+    return_station_name: { type: String, required: true },
+    distance: { type: Number, required: true },
+    duration: { type: Number, required: true },
+  }
+);
+
+//Export model
+module.exports = mongoose.model('Trips', TripsSchema);
