3bee94b531c62bcac4e7363a682b234e3e491552
AholanLate <lauri.ahola2@gmail.com>
2023-05-13 10:13:54 +0300

    frontend gets data and prints

--- client/src/layout/Journeys.vue
+++ client/src/layout/Journeys.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="content">Journeys stuff here</div>
+  <div class="content">
+    <h1>Trips</h1>
+
+      <button @click="loadOne">
+        Load a trip
+      </button>
+      <div>
+        <p v-for="(trip, index) in trips" :key="index">
+          Route: {{ trip.departure_station_name }} to {{ trip.return_station_name }}<br>
+          Start: {{ trip.departure_time }}<br>
+          Duration: {{ trip.duration }}<br>
+          Distance: {{ trip.distance }}m
+        </p>
+      </div>
+  </div>
 </template>
 
-<style>
+<script>
+import axios from 'axios'
 
-.content{
-  text-align: center;
-}
-  
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+export default {
+  data() {
+    return {
+      trips: []
+    }
+  },
+  methods: {
+    loadOne() {
+      axios.get('http://localhost:4000/trips')
+      .then(response => {
+        if (response.data) {
+          this.trips.push(response.data)
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
+}
+}
+</script>
+
+<style>
+.content {
+  text-align: center;
 }
 </style>
--- routes/tripsRoute.js
+++ routes/tripsRoute.js
@@ -1,33 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Trip = require('../models/tripsModel');
+const cors = require('cors');
+
+// Enable CORS
+router.use(cors());
 
 // Fetch all trips
 router.get("/trips", async (req, res) => {
   try {
     const trips = await Trip.findOne();
     res.send(trips)
-  } catch(err) {
-    return res.status(500).json({ message: err.message });
-  }
-})
-
-// Add trip
-router.post("/trips", async (req, res) => {
-  const trip = new Trip({
-    departure_time: req.body.departure_time,
-    return_time: req.body.return_time,
-    departure_station_id: req.body.departure_station_id,
-    departure_station_name: req.body.departure_station_name,
-    return_station_id: req.body.return_station_id,
-    return_station_name: req.body.return_station_name,
-    distance: req.body.distance,
-    duration: req.body.duration
-  });
-
-  try {
-    const newTrip = await trip.save();
-    res.status(201).json({ newTrip });
   } catch(err) {
     return res.status(500).json({ message: err.message });
   }
