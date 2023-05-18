f05a386e9eb8a3f32e74d5acfcd65c8ddafd8b67
AholanLate <lauri.ahola2@gmail.com>
2023-05-18 20:40:21 +0300

    dotenv

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const dotenv = require('dotenv'); // Import the dotenv package
+
+// Load environment variables from .env file
+dotenv.config();
+
 const tripsRoute = require('./routes/tripsRoute');
 const stationsRoute = require('./routes/stationsRoute');
 
 const app = express();
 app.use(bodyParser.json());
+
 // Use the tripsRoute router for all trips-related routes
 app.use('/trips', tripsRoute);
 
@@ -13,7 +19,9 @@ app.use('/stations', stationsRoute);
 
 //MongoDB connection
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb+srv://bikeappuser:bikeapppw@bikeapp.cuw2ogy.mongodb.net/bikeapp?retryWrites=true&w=majority';
+
+// Retrieve MongoDB connection URI from environment variable
+const mongoURL = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURL, { useNewUrlParser: true , useUnifiedTopology: true});
 const db = mongoose.connection;
@@ -22,8 +30,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'));
 const port = 4000;
 
 app.get("/", (req, res) => {
-    res.send("homepage");
-  })
+  res.send("homepage");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
